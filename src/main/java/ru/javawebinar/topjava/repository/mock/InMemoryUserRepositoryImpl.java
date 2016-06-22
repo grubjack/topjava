4723dadf720ebb6e4283567c56ed2422ecc4407b
grubjack <grubjack@gmail.com>
2016-06-22 13:29:38 +0300

    bean life cycle

--- src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserRepositoryImpl.java
@@ -1,9 +1,13 @@
 package ru.javawebinar.topjava.repository.mock;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
@@ -20,6 +24,7 @@ import java.util.stream.Collectors;
 public class InMemoryUserRepositoryImpl implements UserRepository {
     public static final int USER_ID = 1;
     public static final int ADMIN_ID = 2;
+    private static final Logger LOG = LoggerFactory.getLogger(InMemoryUserRepositoryImpl.class);
     private static final Comparator<User> USER_COMPARATOR = Comparator.comparing(User::getName);
     private Map<Integer, User> repository = new ConcurrentHashMap<>();
     private AtomicInteger counter = new AtomicInteger(0);
@@ -32,6 +37,16 @@ public class InMemoryUserRepositoryImpl implements UserRepository {
         }
         repository.put(user.getId(), user);
         return user;
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        LOG.info("+++ PostConstruct");
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        LOG.info("+++ PreDestroy");
     }
 
     @Override
